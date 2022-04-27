38b7df0007f6ed8beaca3cffc02976262c04ffcd
Alex Liu <dsa1314@gmail.com>
2022-04-28 01:28:16 +0800

    chore: initial playground

---
+++ playground/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useForm } from '@vue-composable-form/core'
+
+</script>
+
+<template>
+  <div>
+    <!-- Edit this file to play around with Vue Composable Form but never commit changes! -->
+    Vue Composable Form Playground
+  </div>
+</template>
