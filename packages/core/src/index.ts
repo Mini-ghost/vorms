082004e9502fc3dafd87b138ffa0050672a39b6d
Alex Liu <dsa1314@gmail.com>
2022-05-31 21:12:21 +0800

    feat(core): expose utils function `get` and `set` and type

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,6 @@ export * from './composable/useForm';
 export * from './composable/useField';
 export * from './composable/useFieldArray';
 export { useFormContext } from './composable/useFormContext';
+export { default as set } from './utils/set';
+export { default as get } from './utils/get';
+export * from './types';
