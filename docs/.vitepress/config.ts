1ba8d6ad02139c99093950a18a4664e419438b01
Alex Liu <dsa1314@gmail.com>
2022-07-31 22:41:33 +0800

    docs: update footer

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -85,7 +85,7 @@ export default defineConfig({
 
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2019-present Alex Liu'
+      copyright: 'Copyright © 2022-present Alex Liu'
     },
   }
 })
