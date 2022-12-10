cb80c8205ab0c9682bbb7ddbfe150ae392af46c4
Alex Liu <39984251+Mini-ghost@users.noreply.github.com>
2022-12-10 21:52:23 +0800

    refactor(core): rename `composiable` to `composables` (#8)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,7 @@
-export * from './composable/useForm';
-export * from './composable/useField';
-export * from './composable/useFieldArray';
-export { useFormContext } from './composable/useFormContext';
+export * from './composables/useForm';
+export * from './composables/useField';
+export * from './composables/useFieldArray';
+export { useFormContext } from './composables/useFormContext';
 export { default as set } from './utils/set';
 export { default as get } from './utils/get';
 export * from './types';
