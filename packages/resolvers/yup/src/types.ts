6c3e1b537cba04d1af34aa6912377e054a62b798
Alex Liu <dsa1314@gmail.com>
2024-02-17 19:16:21 +0800

    feat(core): upgrade yup to above v1.0

--- packages/resolvers/yup/src/types.ts
+++ packages/resolvers/yup/src/types.ts
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 import type { FormErrors, FormValues } from '@vorms/core';
-import type Lazy from 'yup/lib/Lazy';
+import type { Lazy } from 'yup';
 
 export type Resolver = <T extends Yup.AnyObjectSchema | Lazy<any>>(
   schema: T,
